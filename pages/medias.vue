8244b131b9ff34790479b6228289c5b5609a15ea
OctoY <yann@octoplus.solutions>
2026-02-04 14:53:02 +0100

    feat: add medias page with project list component and filter mapping

---
+++ pages/medias.vue
@@ -0,0 +1,17 @@
+<template>
+    <app-project-list
+        api-endpoint="medias"
+        :filter-map="tagMap"
+        filter-description="Filtré par type"
+        empty-message="Il n'y a pas encore de média pour"
+    />
+</template>
+
+<script setup lang="ts">
+import AppProjectList from "~/components/AppProjectList.vue";
+
+const tagMap = {
+    "podcast": "Podcast",
+    "video": "Vidéo",
+}
+</script>
