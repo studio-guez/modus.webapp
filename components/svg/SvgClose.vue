7a7a0b82629c7007a381c898f2eb85eee17a37e7
OctoY <yann@octoplus.solutions>
2026-02-22 13:57:06 +0100

    feat: add SvgActualites and SvgClose components for enhanced SVG support

---
+++ components/svg/SvgClose.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
+    <path d="M3.38333 33.8333L0 30.45L13.5333 16.9167L0 3.38333L3.38333 0L16.9167 13.5333L30.45 0L33.8333 3.38333L20.3 16.9167L33.8333 30.45L30.45 33.8333L16.9167 20.3L3.38333 33.8333Z" fill="currentColor"/>
+  </svg>
+</template>
